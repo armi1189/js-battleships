9449a0224d3703a169cdafda6551b81879d2e0d1
armi1189 <armi1189@gmail.com>
2015-04-17 13:57:51 +0100

    cannot overlay ships in board

--- spec/boardSpec.js
+++ spec/boardSpec.js
@@ -3,12 +3,12 @@ describe('Board when is created', function(){
   var board = new Board();
 
   it('has a default size of 2', function(){
-    expect(Object.keys(board.grid).length).toEqual(2);
+    expect(Object.keys(board.grid).length).toEqual(4);
   });
 
   it('can have a variable size', function(){
     board = new Board(10)
-    expect(Object.keys(board.grid).length).toEqual(10);
+    expect(Object.keys(board.grid).length).toEqual(100);
   });
 
 })
@@ -18,34 +18,84 @@ describe('Board after been created', function(){
   beforeEach(function(){
     board = new Board();
     ship = jasmine.createSpyObj('ship', ['size', 'hit', 'hitCount']);
-    first_cell = jasmine.createSpyObj('first_cell', ['content']);
-    second_cell = jasmine.createSpyObj( 'second_cell', ['content']);
+    first_cell = jasmine.createSpyObj('first_cell', ['hit', 'content']);
+    second_cell = jasmine.createSpyObj( 'second_cell', ['hit', 'content']);
     ship.size = 2;
+    first_cell.content = "";
+    second_cell.content = "";
+  });
+
+  it('can work out the horizontal coordinates for a size', function(){
+    expect(getCoords(ship.size, "A1", "horizontal")).toEqual(["A1","A2"]);
+  });
+
+  it('can work out the vertical coordinates for a size', function(){
+    expect(getCoords(ship.size, "A1", "vertical")).toEqual(["A1","B1"]);
   });
 
   it('can place a ship horizontally', function(){
-    board.grid["A"][1] = first_cell
-    board.grid["A"][2] = second_cell
-    board.place(ship, "A", 1, "horizontal")
-    expect(board.grid["A"][2].content).toEqual(ship) 
+    board.grid["A1"] = first_cell
+    board.grid["A2"] = second_cell
+    board.place(ship, "A1", "horizontal")
+    expect(board.grid["A2"].content).toEqual(ship) 
   });
 
   it('can place a ship vertically', function(){
-    board.grid["A"][1] = first_cell
-    board.grid["B"][1] = second_cell
-    board.place(ship, "A", 1, "vertical")
-    expect(board.grid["B"][1].content).toEqual(ship) 
+    board.grid["A1"] = first_cell
+    board.grid["B1"] = second_cell
+    console.log(board.grid["A1"])
+    board.place(ship, "A1", "vertical")
+    expect(board.grid["B1"].content).toEqual(ship) 
   });
+
+  it('knows if a coordinate is on the board', function(){
+    expect(board.checkCoord(["A1", "A2"])).toBe(true);
+  });
+
+  it('knows if a coordinate is not on the board', function(){
+    expect(board.checkCoord(["A2", "A3"])).toBe(false);
+  });
+
+  it('cannot overlay ships', function(){
+    board.grid["A1"] = first_cell
+    board.grid["A2"] = second_cell
+    board.place(ship, "A1", "horizontal")
+    expect(function(){ board.place(ship, "A1", "vertical"); }).toThrow(new Error('Can\'t place a ship there'))
+  });
+
+  it('can\'t place a ship outside of the bounderies', function(){
+    expect(function(){ board.place(ship, "A2", "horizontal"); }).toThrow(new Error('Can\'t place a ship there'))
+  });
+
+  it('can hit items on the board', function(){
+    board.grid["A1"] = first_cell
+    board.grid["A2"] = second_cell
+    board.place(ship, "A1", "horizontal");
+    board.bomb("A1")
+    expect(board.grid["A1"].isHit).toBe(true);
+    expect(board.grid["A1"].content.hitCount).toEqual(1);
+  });
+
+  it('can\'t hit a cell outside of the bounderies', function(){
+    expect(function(){ board.bomb("A3"); }).toThrow(new Error('Cell out of bounds'));
+  });
+
+  it('knows that all ships are sunk', function(){
+    board.grid["A1"] = first_cell
+    board.grid["A2"] = second_cell
+    board.place(ship, "A1", "horizontal");
+    board.bomb("A1");
+    board.bomb("A2");
+    expect(board.checkSunk).toBe(true);
+  });
+
+  it('knows if there are any floating ships', function(){
+    board.place(ship, "A1", "horizontal");
+    expect(board.checkSunk).toBe(false);
+  });
+
+  // it('can fill all the content with water', function(){
+
+  // });
 });  
 
-
-// can work out the coordinates
-// know if a coord is on the board
-// cannot overlay ships
-// cannot place ships out of bounds
-// can bomb coord
-// cannot bomb coord out bounds
-// knows if all ships are sunk
-// knows if there are floating ships
-
-// it can fill all cells with water
--- src/board.js
+++ src/board.js
@@ -1,4 +1,3 @@
-
 function Board(size){
   this.grid = {};
   this.generateGrid(size || 2);
@@ -6,18 +5,18 @@ function Board(size){
 
 Board.prototype.generateGrid = function(size) {
   for(var x=1; x <= size; x++){
-    this.grid[String.fromCharCode(64 + x)] = {};
+    var letter = String.fromCharCode(64 + x)
     for(var y=1; y <= size; y++){
-      this.grid[String.fromCharCode(64 + x)][y] = new Cell();
+      this.grid[letter.concat(y)] = new Cell();
     };
   };
 };
 
-Board.prototype.place = function(ship, coordX, coordY, orient) {
-  var coord = getCoords(ship.size, coordX, coordY, orient);
+Board.prototype.place = function(ship, coord, orient) {
+  var coord = getCoords(ship.size, coord, orient);
+  if (!(this.checkCoord(coord))) throw new Error('Can\'t place a ship there');
   for(i=0; i < coord.length; i++) {
-    if (horizontal(orient)) this.grid[coordX][coord[i]].content = ship;
-    else this.grid[coord[i]][coordY].content = ship;
+    this.grid[coord[i]].content = ship; 
   };
 };
 
@@ -25,12 +24,37 @@ function horizontal(orient){
   return orient === "horizontal";
 };
 
-function getCoords(shipSize, coordX, coordY, orient) {
+function getCoords(shipSize, coord, orient) {
+  coord = coord.split("")
   var coords = [];
-  if (horizontal(orient)) {
-    for(i=0; i<shipSize; i++) coords.push(coordY + i);
-  } else {
-    for(i=0; i<shipSize; i++) coords.push(String.fromCharCode(coordX.charCodeAt(0) + i));
-  }
+  if (horizontal(orient)) coords = getHorizontalCoords(shipSize, coord);
+  else coords = getVerticalCoords(shipSize, coord);
   return coords
+};
+
+function getHorizontalCoords(shipSize, coord) {
+  var coords = [];
+  for(i=0; i<shipSize; i++) {
+    var number = parseInt(coord[1]) + i;
+    coords.push(coord[0].concat(number));
+  }
+  return coords;
+}
+
+function getVerticalCoords(shipSize,coord) {
+  var coords = [];
+  for(i=0; i<shipSize; i++) {
+    var letter = String.fromCharCode(coord[0].charCodeAt(0) + i);
+    coords.push(letter.concat(coord[1]));
+  }
+  return coords;
+}
+
+Board.prototype.checkCoord = function(coords){
+  for (i=0; i < coords.length; i++){
+    var gridKeys = Object.keys(this.grid)
+    if (gridKeys.indexOf(coords[i]) < 0) return false;
+    if (this.grid[coords[i]].content !== "") return false;
+  }
+  return true
 };
